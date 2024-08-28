700b2ec9b8ac4a53759b945f6759d68f847c1b2c
Halit Tiryaki <67932748+halittiryakicom@users.noreply.github.com>
2024-08-28 19:52:34 +0300

    Add files via upload

---
+++ Sql_Odev6.sql
@@ -0,0 +1,11 @@
+-- SORU1
+-- SELECT AVG(rental_rate) FROM film;
+
+-- SORU2
+-- SELECT COUNT(*) FROM film WHERE title ~~'C%';
+
+-- SORU3
+-- SELECT MAX(rental_rate) FROM film;
+
+-- SORU4
+-- SELECT COUNT(DISTINCT replacement_cost) FROM film WHERE length > 150;
