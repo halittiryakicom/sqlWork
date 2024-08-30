72f7f082a58d0356945b0e6b82e084fdf81b67db
Halit Tiryaki <67932748+halittiryakicom@users.noreply.github.com>
2024-08-30 23:46:33 +0300

    Add files via upload

---
+++ Sql_Odev9.sql
@@ -0,0 +1,8 @@
+-- SORU 1
+-- SELECT city.city, country.country  FROM country INNER JOIN city ON country.country_id = city.country_id;
+
+-- SORU 2
+-- SELECT customer.first_name, customer.last_name, payment.payment_id FROM customer INNER JOIN payment ON customer.customer_id = payment.customer_id;
+
+-- SORU 3
+-- SELECT customer.first_name, customer.last_name, rental.rental_id FROM customer INNER JOIN rental ON customer.customer_id = rental.customer_id;
